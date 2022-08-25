b8a25d16f068ec2a93182727adda60ee3d030e26
iamayushy <ayushhuns@gmail.com>
2022-08-25 20:53:48 +0530

    ayush

--- src/components/CustomBox.jsx
+++ src/components/CustomBox.jsx
@@ -1,11 +1,10 @@
-const CustomBox = ({children, boxbody, boxhead, handleColor}) => {
-
-    return(
-        <div onClick={handleColor} className="box">
-            {children}
-            <h3 className="box-head">{boxhead}</h3>
-            <p className="box-body">{boxbody}</p>
-        </div>
-    )
-}
-export {CustomBox}
+const CustomBox = ({ children, boxbody, boxhead, handleColor, color, optionName }) => {
+  return (
+    <div style={{border:`1px solid ${color}`}}  name={optionName} onClick={handleColor} className="box">
+      {children}
+      <h3 className="box-head">{boxhead}</h3>
+      <p className="box-body">{boxbody}</p>
+    </div>
+  );
+};
+export { CustomBox };
