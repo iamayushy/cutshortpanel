b40dcb2489ae692eba49ef0bf845b186febfedbc
iamayushy <ayushhuns@gmail.com>
2022-08-25 11:44:09 +0530

    some functional work

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,8 +1,12 @@
-const InputBox = ({inputName, placeholder, handleInput, values}) => {
+const InputBox = ({inputName, placeholder, handleInput, values, optional='', web=''}) => {
     return(
         <div>
-            <p className="label-text">{inputName}</p>
-            <input onChange={handleInput} value={values} type="text" placeholder={placeholder}/>
+            <p className="label-text">{inputName} <span>{`${optional}`}</span></p>
+            <p className="website">{web}</p>
+            <input onChange={handleInput} value={values}
+             type="text" placeholder={placeholder}
+            />
+
         </div>
     )
 }
--- src/pages/PageTwo.jsx
+++ src/pages/PageTwo.jsx
@@ -3,16 +3,20 @@ import { FormPage } from "../components/FormPage"
 const PageTwo = () => {
     const nextPage = (e) => {
         e.preventDefault()
-        
+
     }
     return(
         <>
+
         <FormPage
         head={"Let's set up a home for all your work"}
         body={'You can always create another workspace later.'}
+        textOne={'Workspace Name'}
+        textTwo={`Workspace URL`}
         moveNext={nextPage}
+        optTwo={'(optional)'}
+        website={'www.eden.com/'}
         />
-
         </>
     )
 }
