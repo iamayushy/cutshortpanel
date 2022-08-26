cf98d1111fb95b472c8e770cbf05102e78b46608
iamayushy <ayushhuns@gmail.com>
2022-08-26 14:52:25 +0530

    completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import eden from '../src/assets/eden.jpeg'
+
 import { StatusPage } from './components/StatusPage'
 import { PageFour } from './pages/PageFour'
 
@@ -7,10 +9,16 @@ import { PageOne } from './pages/PageOne'
 import { PageThree } from './pages/PageThree'
 import { PageTwo } from './pages/PageTwo'
 
+
+
 function App() {
 
   return (
     <div className="App">
+      <section className='logo'>
+        <img src={eden} alt="sss" />
+        <h3>Eden</h3>
+      </section>
       <section className='statusIndicator'>
       <StatusPage/>
       </section>
